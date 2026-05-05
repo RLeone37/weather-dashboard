fe496b70343a83dce1a06fc5b51e2969ce6bf711
RLeone37 <yanks126@gmail.com>
2026-05-04 21:17:06 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,123 +1,46 @@
 // CUT START
-var disableSetup = false; // Manually set to true to disable setup page menu option
+var disableSetup = false;
 var topBarCenterText = "WEATHER DASHBOARD";
 var topBarCenterTextColor = "limegreen";
 
 // Grid layout desired
-var layout_cols = 3;
+var layout_cols = 4;
 var layout_rows = 3;
 
 // Menu items
-// Structure is as follows: HTML Color code, Option, target URL, scaling 1=Original Size, side (optional, nothing is Left, "R" is Right)
-// The values are [color code, menu text, target link, scale factor, side],
-// add new lines following the structure for extra menu options. The comma at the end is important!
 var aURL = [
-  [
-    "2196F3",
-    "APRS",
-    "https://aprs.to/?center=43,-79&zoom=11",
-    "1"
-  ],
-  [
-    "2196F3",
-    "CLUBLOG",
-    "https://clublog.org/livestream/VA3HDL",
-    "1"
-  ],
-  [
-    "2196F3",
-    "CONTEST",
-    "https://www.contestcalendar.com/fivewkcal.html",
-    "1"
-  ],
-  [
-    "2196F3",
-    "DX CLUSTER",
-    "https://dxcluster.ha8tks.hu/map/",
-    "1"
-  ],
-  [
-    "2196F3",
-    "ADSB",
-    "https://globe.adsbexchange.com/",
-    "1",
-    "R"
-  ],
-  [
-    "2196F3",
-    "LIGHTNING",
-    "https://map.blitzortung.org/#3.87/36.5/-89.41",
-    "1",
-    "R"
-  ],
-  [
-    "2196F3",
-    "PISTAR",
-    "http://pi-star.local/",
-    "1"
-  ],
-  [
-    "2196F3",
-    "RADAR",
-    "https://weather.gc.ca/?layers=alert,radar&center=43.39961001,-78.53212031&zoom=6&alertTableFilterProv=ON",
-    "1",
-    "R"
-  ],
-  [
-    "2196F3",
-    "WINLINK",
-    "https://cms.winlink.org:444/maps/propagation.aspx",
-    "1"
-  ],
-  [
-    "2196F3",
-    "Stuff In Space",
-    "https://stuffin.space/",
-    "1",
-    "R"
-  ],
-  [
-    "2196F3",
-    "TIME.IS",
-    "https://time.is/",
-    "1",
-    "R"
-  ],
-  [
-    "2196F3",
-    "WEATHER",
-    "https://openweathermap.org/weathermap?basemap=map&cities=true&layer=temperature&lat=44.0157&lon=-79.4591&zoom=5",
-    "1",
-    "R"
-  ],
-  [
-    "2196F3",
-    "WINDS",
-    "https://earth.nullschool.net/#current/wind/surface/level/orthographic=-78.79,44.09,3000",
-    "1",
-    "R"
-  ]
+  ["2196F3", "APRS",          "https://aprs.to/?center=43,-79&zoom=11",                                                                                         "1"],
+  ["2196F3", "CLUBLOG",       "https://clublog.org/livestream/VA3HDL",                                                                                           "1"],
+  ["2196F3", "CONTEST",       "https://www.contestcalendar.com/fivewkcal.html",                                                                                  "1"],
+  ["2196F3", "DX CLUSTER",    "https://dxcluster.ha8tks.hu/map/",                                                                                                "1"],
+  ["2196F3", "ADSB",          "https://globe.adsbexchange.com/",                                                                                                 "1"],
+  ["2196F3", "LIGHTNING",     "https://map.blitzortung.org/#3.87/36.5/-89.41",                                                                                   "1"],
+  ["2196F3", "PISTAR",        "http://pi-star.local/",                                                                                                           "1"],
+  ["2196F3", "RADAR",         "https://weather.gc.ca/?layers=alert,radar&center=43.39961001,-78.53212031&zoom=6&alertTableFilterProv=ON",                        "1"],
+  ["2196F3", "WINLINK",       "https://cms.winlink.org:444/maps/propagation.aspx",                                                                               "1"],
+  ["2196F3", "Stuff In Space", "https://stuffin.space/",                                                                                                         "1"],
+  ["2196F3", "TIME.IS",       "https://time.is/",                                                                                                                "1"],
+  ["2196F3", "WEATHER",       "https://openweathermap.org/weathermap?basemap=map&cities=true&layer=temperature&lat=44.0157&lon=-79.4591&zoom=5",                 "1"],
+  ["2196F3", "WINDS",         "https://earth.nullschool.net/#current/wind/surface/level/orthographic=-78.79,44.09,3000",                                         "1"]
 ];
 
 // Feed items
-// Structure is as follows: target URL
-// The values are [target link]
 var aRSS = [];
 
-// Dashboard Tiles items
-// Tile Structure is Title, Source URL
-// To display a website on the tiles use "iframe|" keyword before the tile URL
-// [Title, Source URL],
-// the comma at the end is important!
+// Dashboard Tiles — 4 cols x 3 rows = 12 tiles, left-to-right, top-to-bottom
+// Row 1: US Radar | SE Radar | Lightning | Fire Outlook 7-day
+// Row 2: US Satellite | SE Satellite | Drought Monitor | National Forecast
+// Row 3: E Pacific 2-day | E Pacific 7-day | Atlantic 2-day | Atlantic 7-day
 var aIMG = [
+
+  // Row 1
   [
-    "RADAR US/SE",
-    "https://radar.weather.gov/ridge/standard/CONUS_loop.gif",
-    "https://radar.weather.gov/ridge/standard/KTBW_loop.gif"
+    "US RADAR",
+    "https://radar.weather.gov/ridge/standard/CONUS_loop.gif"
   ],
   [
-    "NATIONAL FORECAST",
-    "https://www.wpc.ncep.noaa.gov/noaa/noaad3.gif?1755916220"
+    "SE RADAR",
+    "https://radar.weather.gov/ridge/standard/KTBW_loop.gif"
   ],
   [
     "LIGHTNING",
@@ -125,7 +48,7 @@ var aIMG = [
     "https://www.blitzortung.org/en/Images/image_b_fl.png"
   ],
   [
-    "WILDLAND FIRE POTENTIAL 7-DAY OUTLOOK",
+    "WILDLAND FIRE OUTLOOK 7-DAY",
     "https://www.nifc.gov/modules/custom/nicc_outlooks/images/outlook_d1.png",
     "https://www.nifc.gov/modules/custom/nicc_outlooks/images/outlook_d2.png",
     "https://www.nifc.gov/modules/custom/nicc_outlooks/images/outlook_d3.png",
@@ -134,42 +57,61 @@ var aIMG = [
     "https://www.nifc.gov/modules/custom/nicc_outlooks/images/outlook_d6.png",
     "https://www.nifc.gov/modules/custom/nicc_outlooks/images/outlook_d7.png"
   ],
+
+  // Row 2
   [
-    "SATELLITE US/SE",
-    "https://cdn.star.nesdis.noaa.gov/GOES19/ABI/CONUS/GEOCOLOR/GOES19-CONUS-GEOCOLOR-625x375.gif",
+    "US SATELLITE",
+    "https://cdn.star.nesdis.noaa.gov/GOES19/ABI/CONUS/GEOCOLOR/GOES19-CONUS-GEOCOLOR-625x375.gif"
+  ],
+  [
+    "SE SATELLITE",
     "https://cdn.star.nesdis.noaa.gov/GOES19/ABI/SECTOR/se/GEOCOLOR/GOES19-SE-GEOCOLOR-600x600.gif"
   ],
   [
-    "EASTERN PACIFIC TROPICAL OUTLOOK 2/7 DAY",
-    "https://www.nhc.noaa.gov/archive/xgtwo/epac/latest/two_pac_2d0.png",
+    "US DROUGHT MONITOR",
+    "https://droughtmonitor.unl.edu/data/png/current/current_usdm.png"
+  ],
+  [
+    "NATIONAL FORECAST",
+    "https://www.wpc.ncep.noaa.gov/noaa/noaad1.gif",
+    "https://www.wpc.ncep.noaa.gov/noaa/noaad2.gif",
+    "https://www.wpc.ncep.noaa.gov/noaa/noaad3.gif"
+  ],
+
+  // Row 3
+  [
+    "E PACIFIC TROPICAL 2-DAY",
+    "https://www.nhc.noaa.gov/archive/xgtwo/epac/latest/two_pac_2d0.png"
+  ],
+  [
+    "E PACIFIC TROPICAL 7-DAY",
     "https://www.nhc.noaa.gov/archive/xgtwo/epac/latest/two_pac_7d0.png"
   ],
   [
-    "ATLANTIC TROPICAL OUTLOOK 2/7 DAY",
-    "https://www.nhc.noaa.gov/archive/xgtwo/atl/latest/two_atl_2d0.png",
+    "ATLANTIC TROPICAL 2-DAY",
+    "https://www.nhc.noaa.gov/archive/xgtwo/atl/latest/two_atl_2d0.png"
+  ],
+  [
+    "ATLANTIC TROPICAL 7-DAY",
     "https://www.nhc.noaa.gov/archive/xgtwo/atl/latest/two_atl_7d0.png"
-  ],
-  [
-    "WILDLAND FIRE POTENTIAL MONTHLY OUTLOOK",
-    "https://www.nifc.gov/nicc-files/predictive/outlooks/month1_outlook.png"
-  ],
-  [
-    "EARTHQUAKES 2.5+ PAST 7 DAYS",
-    "iframe|https://www.cisn.org/map/"
   ]
+
 ];
 
-// Image rotation intervals in milliseconds per tile - If the line below is commented, tiles will be rotated every 5000 milliseconds (5s)
+// Rotation intervals in ms — must be 12 entries to match aIMG
 var tileDelay = [
-  15000,
-  15100,
-  15200,
-  10000,
-  15400,
-  15500,
-  15600,
-  15700,
-  15800
+  15000,  // US Radar
+  15000,  // SE Radar
+  15000,  // Lightning (2 images)
+  10000,  // Fire Outlook (cycles 7 days)
+  15000,  // US Satellite
+  15000,  // SE Satellite
+  60000,  // Drought Monitor (static, updates weekly)
+  15000,  // National Forecast (cycles d1/d2/d3)
+  30000,  // E Pacific 2-day
+  30000,  // E Pacific 7-day
+  30000,  // Atlantic 2-day
+  30000   // Atlantic 7-day
 ];
 
 // CUT END
